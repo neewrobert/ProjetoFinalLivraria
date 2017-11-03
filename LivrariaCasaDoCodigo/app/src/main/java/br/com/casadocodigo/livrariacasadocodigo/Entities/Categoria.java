ab2a1dab184f9f7003be93df97b5ccbaf428e86b
Newton Roberto dos Santos <nrdossantos@indracompany.com>
2017-11-03 12:33:52 -0200

    Finalizacao da tela de catalogo, inicio da criacao da tela de cadastro de livro, Criacao das entidades Livro e Catalogo

---
+++ LivrariaCasaDoCodigo/app/src/main/java/br/com/casadocodigo/livrariacasadocodigo/Entities/Categoria.java
@@ -0,0 +1,49 @@
+package br.com.casadocodigo.livrariacasadocodigo.Entities;
+
+import java.io.Serializable;
+
+/**
+ * Created by nrdossantos on 03/11/2017.
+ */
+
+class Categoria implements Serializable {
+
+    private Long id;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return getNome();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        return nome.equalsIgnoreCase(categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return nome.hashCode();
+    }
+}
--- LivrariaCasaDoCodigo/app/src/main/java/br/com/casadocodigo/livrariacasadocodigo/HomeActivity.java
+++ LivrariaCasaDoCodigo/app/src/main/java/br/com/casadocodigo/livrariacasadocodigo/HomeActivity.java
@@ -1,13 +1,17 @@
 package br.com.casadocodigo.livrariacasadocodigo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button btnCatalogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,18 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent vaiCadastroLivro = new Intent(HomeActivity.this, CadastrarLivroActivity.class);
+                startActivity(vaiCadastroLivro);
+            }
+        });
+
+        btnCatalogo = (Button) findViewById(R.id.adm_btnCatalogo);
+        btnCatalogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentVaiCatalogo = new Intent(HomeActivity.this, CatalogoActivity.class);
+                startActivity(intentVaiCatalogo);
             }
         });
     }
